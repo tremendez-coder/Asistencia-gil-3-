aa74ea3915e81a5e58d954faf561d451e9292f66
Vicc087 <crespinvic.087@gmail.com>
2025-08-07 13:05:50 -0300

    Delete Base de datos directory

--- Base de datos/eliminarfotos.py
+++
@@ -1,9 +0,0 @@
-import os
-
-carpeta_faces = 'recognizer/faces'
-
-for archivo in os.listdir(carpeta_faces):
-    if archivo.endswith(".jpg"):
-        os.remove(os.path.join(carpeta_faces, archivo))
-
-print("[INFO] Se borraron todas las imágenes de entrenamiento.")
